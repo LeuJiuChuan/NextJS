e1738eafbd37635c841209ad439323dfc71c0d9b
LeuJiuChuan <jiuc0474@gmail.com>
2023-12-13 20:36:05 +0800

    Update section 5

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -22,30 +22,42 @@ export async function createSnippet(
   formState: { message: string },
   formData: FormData
 ) {
+  try {
+    //   check the user's inpurt and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-//   check the user's inpurt and make sure they're valid
-  const title = formData.get('title') 
-  const code = formData.get('code') 
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title nust be longer",
+      };
+    }
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "code must be longer",
+      };
+    }
+    //create a new  record in the database
+    await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
 
-  if(typeof title !== 'string' || title.length<3){
-    return{
-        message:'Title nust be longer'
+    // throw new Error('Failed to save to database!!!!') withotu this it wont show any error page
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        message: error.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
     }
   }
-  if(typeof code !== 'string' || code.length<10){
-    return{
-        message:'code must be longer'
-    }
-  }
-  //create a new  record in the database
-  const snippet=await db.snippet.create({
-      data:{
-          title:title,
-          code:code
-      }
-  });
-  console.log(snippet);
-
-//   Redirect the user back to the root route
+  //   Redirect the user back to the root route
+  //never put redirect statement in trycatch
   redirect("/");
 }
---
+++ snippets/src/app/snippets/new/error.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+interface ErrorPageProps {
+    error:Error,
+    reset:()=>void;
+}
+
+export default function ErrorPage({error}:ErrorPageProps) {
+  return (
+    <div>{error.message}</div>
+  )
+}
